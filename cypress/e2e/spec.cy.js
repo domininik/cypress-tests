6cb8aaedef51c1143751faa5fef4d1af42dd82fe
domininik <dominik.fijas+github@gmail.com>
2023-07-26 20:36:54 +0200

    Initialize API tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('API spec', () => {
+  context('POST /', function () {
+    it('responds succesfully', function () {
+      cy.request({
+        method: 'POST', 
+        url: 'http://localhost:8080/', 
+        body: { 
+          id: 0, 
+          data: { from: 'ETH', to: 'USD' } 
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    })
   })
 })
