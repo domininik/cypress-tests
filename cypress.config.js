71645bdf3ee5bb5d8d68b26bdf090bba65fdfef4
domininik <dominik.fijas+github@gmail.com>
2023-07-26 21:23:51 +0200

    Add ability to log into terminal output

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,15 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      // cy.task('log', 'This will be output to the terminal')
+      on('task', {
+        log(message) {
+          console.log(message)
+
+          return null
+        },
+      })
     },
     video: false,
     screenshotOnRunFailure: false,
