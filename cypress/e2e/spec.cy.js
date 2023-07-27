9bded434136e219b59340e94c9391d66931de072
domininik <dominik.fijas+github@gmail.com>
2023-07-27 10:13:01 +0200

    Add some more tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -33,5 +33,125 @@ describe('API spec', () => {
         })
       })
     })
+
+    context('base and quote in params', () => {
+      it('responds with correct data', () => {
+        cy.request('POST', '/', { 
+          data: { base: 'ETH', quote: 'USD' }
+        }).then((response) => {
+          expect(response.body.data.USD).to.be.greaterThan(0)
+          expect(response.body.data.result).to.be.greaterThan(0)
+        })
+      })
+    })
+
+    context('coin and market in params', () => {
+      it('responds with correct data', () => {
+        cy.request('POST', '/', { 
+          data: { coin: 'ETH', market: 'USD' }
+        }).then((response) => {
+          expect(response.body.data.USD).to.be.greaterThan(0)
+          expect(response.body.data.result).to.be.greaterThan(0)
+        })
+      })
+    })
+
+    context('from param not supplied', () => {
+      it('responds with error', () => {
+        cy.request({
+          method: 'POST',
+          url: '/',
+          failOnStatusCode: false,
+          body: { data: { to: 'USD' } }
+        }).then((response) => {
+          expect(response.status).to.eq(500)
+          expect(response.body.error.error.message.error.message).to.eq('Required parameter not supplied: base')
+        })
+      })
+    })
+
+    context('to param not supplied', () => {
+      it('responds with error', () => {
+        cy.request({
+          method: 'POST',
+          url: '/',
+          failOnStatusCode: false,
+          body: { data: { from: 'ETH' } }
+        }).then((response) => {
+          expect(response.status).to.eq(500)
+          expect(response.body.error.error.message.error.message).to.eq('Required parameter not supplied: quote')
+        })
+      })
+    })
+
+    context('base param not supplied', () => {
+      it('responds with error', () => {
+        cy.request({
+          method: 'POST',
+          url: '/',
+          failOnStatusCode: false,
+          body: { data: { quote: 'USD' } }
+        }).then((response) => {
+          expect(response.status).to.eq(500)
+          expect(response.body.error.error.message.error.message).to.eq('Required parameter not supplied: base')
+        })
+      })
+    })
+
+    context('quote param not supplied', () => {
+      it('responds with error', () => {
+        cy.request({
+          method: 'POST',
+          url: '/',
+          failOnStatusCode: false,
+          body: { data: { base: 'ETH' } }
+        }).then((response) => {
+          expect(response.status).to.eq(500)
+          expect(response.body.error.error.message.error.message).to.eq('Required parameter not supplied: quote')
+        })
+      })
+    })
+
+    context('coin param not supplied', () => {
+      it('responds with error', () => {
+        cy.request({
+          method: 'POST',
+          url: '/',
+          failOnStatusCode: false,
+          body: { data: { market: 'USD' } }
+        }).then((response) => {
+          expect(response.status).to.eq(500)
+          expect(response.body.error.error.message.error.message).to.eq('Required parameter not supplied: base')
+        })
+      })
+    })
+
+    context('market param not supplied', () => {
+      it('responds with error', () => {
+        cy.request({
+          method: 'POST',
+          url: '/',
+          failOnStatusCode: false,
+          body: { data: { coin: 'ETH' } }
+        }).then((response) => {
+          expect(response.status).to.eq(500)
+          expect(response.body.error.error.message.error.message).to.eq('Required parameter not supplied: quote')
+        })
+      })
+    })
+
+    context('empty params', () => {
+      it('responds with error', () => {
+        cy.request({
+          method: 'POST',
+          url: '/',
+          failOnStatusCode: false,
+          body: { data: {} }
+        }).then((response) => {
+          expect(response.status).to.eq(500)
+          expect(response.body.error.error.message.error.message).to.eq('Required parameter not supplied: base')
+        })
+      })
+    })
   })
 })
